d50e3299ede67568c5c059724fb496e1c8ae739a
Leirmo <daniel.leirmo@hotmail.com>
2022-12-06 17:23:45 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+//Question 1
+var pets = [
+  {
+    type: "cat",
+    age: 5.5,
+  },
+  {
+    type: "dog",
+    age: 3.8,
+  },
+  {
+    type: "parrot",
+    age: 4.0,
+  },
+];
+
+for (var i = 0; i < pets.length; i++) {
+  var age = pets[i].age;
+  var type = pets[i].type;
+
+  if (age >= 4.0) {
+    console.log(type);
+  }
+}
+
+//Question 2
+
+function logWord(Word) {
+  console.log(word);
+}
+
+logWord(true);
+
+//Question 3
+var heading = document.querySelector("h2");
+var button = document.querySelector("button");
+
+function changeHeading() {
+  var h2 = document.querySelector("h2");
+  h2.style.color = "blue";
+  heading.innerHTML = "Updated subheading";
+}
+
+button.onclick = changeHeading;
